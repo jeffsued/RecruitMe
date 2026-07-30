794923a4c6e20d4e51a97e3d96f4883edae67e93
jeffsued <dev.jeffersonsued@gmail.com>
2026-07-30 15:47:53 -0300

    implementação de Candidato e Implementação

--- src/Candidato.java
+++ src/Candidato.java
@@ -1,10 +1,12 @@
+import java.util.Objects;
+
 public class Candidato{
     private String email;
     private String nome;
     private double pontosXp;
     
 
-    public Candidato(String n, String e,int xp){
+    public Candidato(String n, String e,double xp){
         this.email = e;
         this.nome = n;
         this.pontosXp = xp;
@@ -22,8 +24,24 @@ public class Candidato{
 
     @Override
     public String toString() {
-        return ;
+        return nome + " - " + email + " (" + pontosXp + " pts)";
     }
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(email);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Candidato other = (Candidato) obj;
+		return Objects.equals(email, other.email);
+	}
 
 }
--- src/Oportunidade.java
+++ src/Oportunidade.java
@@ -1,7 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Oportunidade {
+	
     private String nomeProjeto;
-    private boolean status;
+    private String status;
     private double pontos;
     private int qtdPessoas;
+    private List<Candidato> selecionados;
+	
+    public Oportunidade(String nomeProjeto, int qtdPessoas, double pontos) {
+        this.nomeProjeto = nomeProjeto;
+        this.qtdPessoas = qtdPessoas;
+        this.pontos = pontos;
+        this.status = "aberta";            
+        this.selecionados = new ArrayList<>();   
+    }
+    
+    public boolean adicionarCandidato(Candidato candidato) {
+    	if (this.status.equals("fechada")) {
+    	    return false;
+    	}
+    	if(candidato.getPontosXp() < this.pontos) {
+    		return false;
+    	}
+    	if (this.selecionados.contains(candidato)) {
+    	    return false;
+    	}
+    	
+    	this.selecionados.add(candidato);
+    	
+    	if(selecionados.size()== this.qtdPessoas) {
+    		 this.status = "fechada";
+    	}
+    	
+		return true;
+    }
+    
+    public String getNomeProjeto() {
+        return nomeProjeto;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public List<Candidato> getSelecionados() {
+        return selecionados;
+    }
     
 }
